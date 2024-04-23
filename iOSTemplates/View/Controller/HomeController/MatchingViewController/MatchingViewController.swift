0eb40e1351eea2b795e5ba08c1b986d20466f7e4
haotv <haotv_tts@rikkeisoft.com>
2024-04-23 10:08:56 +0700

    feat: add ui UIVisualEffectView in MatchingView: accept, close Job

--- iOSTemplates/View/Controller/HomeController/MatchingViewController/MatchingViewController.swift
+++ iOSTemplates/View/Controller/HomeController/MatchingViewController/MatchingViewController.swift
@@ -10,13 +10,18 @@ final class MatchingViewController: ViewController {
         case isHide
         case isShow
     }
+    //MARK: - Constants
+    private enum Const {
+        static let acceptText: String = "Lướt qua phải để tuyển dụng"
+        static let closeText: String = "Lướt qua trái để bỏ qua"
+    }
     //MARK: - IBOutlets
     @IBOutlet private weak var kolodaView: KolodaView!
 
     //MARK: - Propeties
     var viewModel: MatchingViewModel?
     private var blurredEffectView = UIVisualEffectView(effect: .none)
-    var userSwiped = false
+    private var textLabel = UILabel()
     weak var delegate: MatchingViewControllerDelegate?
     //MARK: - Functions
     override func setupUI() {
@@ -25,7 +30,7 @@ final class MatchingViewController: ViewController {
 
     override func setupData() {
         super.setupData()
-        guard let viewModel = viewModel else { return  }
+        guard let viewModel = viewModel else { return }
         viewModel.listPost()
         kolodaView.delegate = self
         kolodaView.dataSource = self
@@ -35,10 +40,12 @@ final class MatchingViewController: ViewController {
 //MARK: - KolodaViewDelegate, KolodaViewDataSource
 extension MatchingViewController: KolodaViewDelegate, KolodaViewDataSource {
     func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
-        kolodaView.reloadData()
+        guard let viewModel = self.viewModel else { return }
+        viewModel.listPost2()
+        kolodaView.resetCurrentCardIndex()
+        
     }
     func kolodaNumberOfCards(_ koloda: Koloda.KolodaView) -> Int {
-        
         return viewModel?.posts.count ?? 0
     }
     func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
@@ -54,6 +61,34 @@ extension MatchingViewController: KolodaViewDelegate, KolodaViewDataSource {
         return view
     }
     func koloda(_ koloda: KolodaView, draggedCardWithPercentage finishPercentage: CGFloat, in direction: SwipeResultDirection) {
+        if let currentCardView = kolodaView.viewForCard(at: kolodaView.currentCardIndex) {
+            switch direction {
+            case .left, .bottomLeft, .topLeft:
+                let redColor = UIColor.hexStringToUIColor(hex: "#F5222D")
+                textLabel.text = ""
+                currentCardView.addUIEffect(withBlurredView: blurredEffectView, color: redColor, nameIcon: NameIcon.icon_closeRed, text: Const.closeText, label: textLabel)
+            default:
+                let greenColor = UIColor.hexStringToUIColor(hex: "#52C41A")
+                textLabel.text = ""
+                currentCardView.addUIEffect(withBlurredView: blurredEffectView, color: greenColor, nameIcon: NameIcon.icon_acceptGreen, text: Const.acceptText, label: textLabel)
+            }
+        }
+    }
+    func koloda(_ koloda: KolodaView, didSwipeCardAt index: Int, in direction: SwipeResultDirection) {
+        switch direction {
+        case .left:
+            guard let viewModel = viewModel else { return }
+            viewModel.removePost(at: index)
+            kolodaView.resetCurrentCardIndex()
+        default:
+            return
+        }
+       
+    }
+    func kolodaDidResetCard(_ koloda: KolodaView) {
+        if kolodaView.viewForCard(at: kolodaView.currentCardIndex) != nil {
+            blurredEffectView.removeUIBlurEffect(blurredEffectView: blurredEffectView)
+        }
     }
 }
 
@@ -88,11 +123,31 @@ extension MatchingViewController: PostDetailViewControllerDelegate {
     }
 }
 
-extension KolodaView {
-    func addUIEffect(withColor color: UIColor,blurredEffectView: UIVisualEffectView) {
-        blurredEffectView.backgroundColor = color.withAlphaComponent(0.16)
-        blurredEffectView.frame = self.bounds
-        self.addSubview(blurredEffectView)
+extension UIView {
+    func addUIEffect(withBlurredView blurredView: UIVisualEffectView, color: UIColor, nameIcon: String , text: String, label: UILabel) {
+        blurredView.backgroundColor = color.withAlphaComponent(0.16)
+        blurredView.frame = self.bounds
+
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: nameIcon), for: .normal)
+        blurredView.contentView.addSubview(button)
+
+        button.heightAnchor(60)
+        button.widthAnchor(60)
+        button.centerVerticallyToSuperView()
+        button.centerHorrizontallyToSuperView()
+
+        label.text = text
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        blurredView.contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 10),
+            label.centerXAnchor.constraint(equalTo: blurredView.contentView.centerXAnchor)
+            ])
+        
+        self.addSubview(blurredView)
     }
 
     func removeUIBlurEffect(blurredEffectView: UIVisualEffectView) {
